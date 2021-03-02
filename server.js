e6e3e4814afb37af1d02f7879370d2e509f1680e
sean-mcinturff <sean4227@live.com>
2021-03-02 08:55:33 -0500

    added several routes & database models

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const { setRandomFallback } = require('bcryptjs');
 const express = require('express');
+const connectDB = require('./config/db')
 
 const app = express();
 
-app.get('/', (req, res) => res.send('API Running'))
+// Connect database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/users', require('./Routes/api/users'));
+app.use('/api/auth', require('./Routes/api/auth'));
+app.use('/api/profile', require('./Routes/api/profile'))
+app.use('/api/posts', require('./Routes/api/posts'))
 
 const PORT = process.env.PORT || 5000;
 
