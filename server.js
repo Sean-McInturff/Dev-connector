eae02b539065c78cb28e7d09cc9f0b0181df565e
sean-mcinturff <sean4227@live.com>
2021-03-10 16:19:08 -0500

    prepare to deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const { setRandomFallback } = require('bcryptjs');
 const express = require('express');
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
+const path = require('path')
 
 const app = express();
 
@@ -10,13 +11,21 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/users', require('./Routes/api/users'));
 app.use('/api/auth', require('./Routes/api/auth'));
-app.use('/api/profile', require('./Routes/api/profile'))
-app.use('/api/posts', require('./Routes/api/posts'))
+app.use('/api/profile', require('./Routes/api/profile'));
+app.use('/api/posts', require('./Routes/api/posts'));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
